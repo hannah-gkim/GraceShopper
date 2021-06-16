adc3620e699cadf25161479871b67e04f5b0bf6d
hannahk0108 <hannahk0108@gmail.com>
2021-06-16 16:31:51 -0500

    create branch: created checkout-cart branch

---
+++ client/components/CheckoutCart.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+/*
+create checkout component where user can view all chosen products
+ show name, image, price, delete,
+ change quantity dropdown menu
+ checkout box that contains total price, checkout button
+*/
+
+class CheckoutCart extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Shopping bag</h1>
+        <h2>image</h2>
+        <h2>name</h2>
+        <h2>price</h2>
+        <h2></h2>
+      </div>
+    );
+  }
+}
+
+export default CheckoutCart;
