c5464e2278d7adca534274a15fe88fdcd726fb60
a-hsu <a.hsu873@gmail.com>
2021-06-16 16:17:25 -0400

    feat: All Products Page T1 Slice (no styling, no pictures)

--- client/Routes.js
+++ client/Routes.js
@@ -10,51 +10,51 @@ import AllProducts from "./components/AllProducts";
  * COMPONENT
  */
 class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData();
-  }
+    componentDidMount() {
+        this.props.loadInitialData();
+    }
 
-  render() {
-    const { isLoggedIn } = this.props;
+    render() {
+        const { isLoggedIn } = this.props;
 
-    return (
-      <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Redirect to="/home" />
-            {/* <Route path="/products" component={AllProducts} /> */}
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/products" component={AllProducts} />
-          </Switch>
-        )}
-      </div>
-    );
-  }
+        return (
+            <div>
+                {isLoggedIn ? (
+                    <Switch>
+                        <Route path="/home" component={Home} />
+                        <Redirect to="/home" />
+                        {/* <Route path="/products" component={AllProducts} /> */}
+                    </Switch>
+                ) : (
+                    <Switch>
+                        <Route path="/" exact component={Login} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/signup" component={Signup} />
+                        <Route exact path="/products" component={AllProducts} />
+                    </Switch>
+                )}
+            </div>
+        );
+    }
 }
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id,
-  };
+    return {
+        // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+        // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+        isLoggedIn: !!state.auth.id,
+    };
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(me());
-    },
-  };
+    return {
+        loadInitialData() {
+            dispatch(me());
+        },
+    };
 };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,50 +1,58 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getProducts } from "../store/allProducts";
+import { Link } from "react-router-dom";
 
 class AllProducts extends React.Component {
-  constructor(props) {
-    super(props);
-    // this.state = { products: [] };
-  }
+    constructor(props) {
+        super(props);
+        // this.state = { products: [] };
+    }
 
-  componentDidMount() {
-    this.props.loadAllProducts();
-  }
+    componentDidMount() {
+        this.props.loadAllProducts();
+    }
 
-  // componentDidUpdate(prevProps) {
-  //   //conditions
-  //   if (prevProps !== this.props) {
-  //     this.setState(this.props);
-  //   }
-  // }
-  render() {
-    console.log(this.props);
-    const { products } = this.props;
-    return (
-      <div>
-        <ul>
-          {products
-            ? products.map((product) => {
-                return <li key={product.id}> {product.name}</li>;
-              })
-            : "no products"}
-        </ul>
-      </div>
-    );
-  }
+    // componentDidUpdate(prevProps) {
+    //   //conditions
+    //   if (prevProps !== this.props) {
+    //     this.setState(this.props);
+    //   }
+    // }
+    render() {
+        console.log(this.props);
+        const { products } = this.props;
+        return (
+            <div>
+                <ul>
+                    {products
+                        ? products.map((product) => {
+                              return (
+                                  <Link
+                                      to={`/products/${product.id}`}
+                                      key={product.id}
+                                  >
+                                      <li> {product.name}</li>
+                                  </Link>
+                              );
+                          })
+                        : "no products"}
+                </ul>
+            </div>
+        );
+    }
 }
 
 const mapState = (state) => {
-  return {
-    products: state.products,
-  };
+    return {
+        products: state.products,
+    };
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    loadAllProducts: () => dispatch(getProducts()),
-  };
+    return {
+        loadAllProducts: () => dispatch(getProducts()),
+    };
 };
 
 export default connect(mapState, mapDispatch)(AllProducts);
