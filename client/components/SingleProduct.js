07a34299d6a552ec2bc334af89321bca83d8a955
hannahk0108 <hannahk0108@gmail.com>
2021-06-16 15:28:12 -0500

    added: products name, image, description, price, quantity dropdown menu

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -3,37 +3,49 @@ import { connect } from "react-redux";
 import { fetchSingleProduct } from "../store/singleProduct";
 
 class SingleProduct extends Component {
-    // constructor() {
-    //     super();
-    // }
-    componentDidMount() {
-        this.props.loadProduct(this.props.match.params.id);
-    }
-    // componentWillUnmount() {
-    //     this.props.student.id = undefined;
-    // }
-    render() {
-        const product = this.props.product || {};
-        console.log(this.props.product);
-        return (
-            <div>
-                <h1>Single Product Page</h1>
-                <h3>name: {product.name}</h3>
-            </div>
-        );
-    }
+  // constructor() {
+  //     super();
+  // }
+  componentDidMount() {
+    this.props.loadProduct(this.props.match.params.id);
+  }
+  // componentWillUnmount() {
+  //     this.props.student.id = undefined;
+  // }
+  render() {
+    const product = this.props.singleProduct || {};
+    const quantity = this.props.singleProduct.quantity;
+
+    return (
+      <div>
+        <h1>Single Product Page</h1>
+        <img src={product.imageUrl} />
+        <h3>name: {product.name}</h3>
+        <h3>price: {`$ ${product.price}`}</h3>
+        <h3>description: {product.description}</h3>
+
+        <select value="" onChange="">
+          <option value="">Qty:1</option>
+          <option value="">2</option>
+          <option value="">3</option>
+          <option value="">4</option>
+        </select>
+        <button>add to cart</button>
+      </div>
+    );
+  }
 }
 
-const mapState = ({ product }) => {
-    return { product };
+const mapState = ({ singleProduct }) => {
+  return { singleProduct };
 };
 
 const mapDispatch = (dispatch) => {
-    return {
-        loadProduct: (id) => {
-            dispatch(fetchSingleProduct(id));
-        },
-    };
+  return {
+    loadProduct: (id) => {
+      dispatch(fetchSingleProduct(id));
+    },
+  };
 };
 
 export default connect(mapState, mapDispatch)(SingleProduct);
