a9d12d8fd50c9092b3bfc75ead9f8079adad42b6
a-hsu <a.hsu873@gmail.com>
2021-06-16 12:23:43 -0400

    feat: added single product backend route

--- server/api/products.js
+++ server/api/products.js
@@ -1,17 +1,26 @@
 const router = require("express").Router();
 const {
-  models: { Product },
+    models: { Product },
 } = require("../db");
 module.exports = router;
 
+// GET /products
+router.get("/", async (req, res, next) => {
+    try {
+        const products = await Product.findAll();
+        res.status(200).json(products);
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.get('/',async(req, res, next)=>{
-
-  try {
-    const products = await Product.findAll()
-    res.status(200).json(products)
-  } catch (error) {
-    next(error)
-  }
-
-})
+// GET /products/:id
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.body;
+        const product = await Campus.findByPk(id);
+        res.json(product);
+    } catch (error) {
+        next(error);
+    }
+});
