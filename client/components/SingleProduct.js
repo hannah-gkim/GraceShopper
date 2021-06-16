1fd2b0b1851a0eccd0e0b505116716cdc0f82806
hannahk0108 <hannahk0108@gmail.com>
2021-06-16 12:12:06 -0500

    still working on SingleProduct.js

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,33 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchSingleProduct } from "../store/singleProduct";
 
-class SingleProduct extends React.Component() {
-    constructor(props) {
-        super(props);
-    }
-    componentDidMount() {
-        this.props.loadProduct(this.props.match.params.id);
-    }
-    render() {
-        return (
-            <div>
-                <h1>Single Product Page</h1>
-            </div>
-        );
-    }
+class SingleProduct extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    // this.props.loadProduct(this.props.match.params.id);
+  }
+  render() {
+    // const product = this.props.product;
+    return (
+      <div>
+        <h1>Single Product Page</h1>
+      </div>
+    );
+  }
 }
 
 const mapState = (state) => {
-    return {
-        product: state.product,
-    };
+  return {
+    product: state.product,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-    loadProduct: (id) => {
-        dispatch(fetchSingleProduct(id));
-    };
+  loadProduct: (id) => {
+    dispatch(fetchSingleProduct(id));
+  };
 };
 
 export default connect(mapState, mapDispatch)(SingleProduct);
