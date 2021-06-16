802ce296db98ff11c4cfad80771ee9d49ea74047
hannahk0108 <hannahk0108@gmail.com>
2021-06-16 16:55:13 -0500

    added: checkout cart component fronted edited

--- client/components/CheckoutCart.js
+++ client/components/CheckoutCart.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
 
-/*
-
- change quantity dropdown menu
- checkout box that contains total price, checkout button
-*/
-
 class CheckoutCart extends Component {
   render() {
     return (
       <div>
         {/* need to be able to see all items, so map it! */}
         <h1>Shopping bag</h1>
-        <h2>image</h2>
-        <h2>name</h2>
-        <h2>price</h2>
-        <button>delete</button>
-        <select value="" onChange="">
-          <option value="">1</option>
-          <option value="">2</option>
-          <option value="">3</option>
-          <option value="">4</option>
-        </select>
+        <div className="checkout-items">
+          <h2>image</h2>
+          <h2>name</h2>
+          <h2>price</h2>
+          <button>delete</button>
+          <select value="" onChange="">
+            <option value="">1</option>
+            <option value="">2</option>
+            <option value="">3</option>
+            <option value="">4</option>
+          </select>
+        </div>
+        <div className="checkout-box">
+          <h3>total $...</h3>
+          <button>CONTINUE TO CHECKOUT</button>
+        </div>
       </div>
     );
   }
