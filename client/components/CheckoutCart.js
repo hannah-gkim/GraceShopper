91f33e85b98eba361cf3a6cbbdc825313ce0ebfb
hannahk0108 <hannahk0108@gmail.com>
2021-06-17 14:00:16 -0500

    inProgress: working on checkout cart flex-box CSS

--- client/components/CheckoutCart.js
+++ client/components/CheckoutCart.js
@@ -5,23 +5,34 @@ class CheckoutCart extends Component {
     return (
       <div>
         {/* need to be able to see all items, so map it! */}
-        <h1>Shopping bag</h1>
-        <div className="checkout-items">
-          <h2>image</h2>
-          <h2>name</h2>
-          <h2>price</h2>
-          <button>delete</button>
-          <select value="" onChange="">
-            <option value="">1</option>
-            <option value="">2</option>
-            <option value="">3</option>
-            <option value="">4</option>
-          </select>
-        </div>
-        <div className="checkout-box">
-          <h3>total $...</h3>
-          <button>CONTINUE TO CHECKOUT</button>
-        </div>
+
+        <box className="checkout-div">
+          <div className="checkout-items-box">
+            <h1 className="title">Shopping Cart</h1>
+            <div className="somebox">
+              <div className="singeitem-image">
+                <img src="https://blog.williams-sonoma.com/wp-content/uploads/2018/06/jun-23-Vanilla-Ombre-Layer-Cake.jpg" />
+                <div className="detail">
+                  <h2>name</h2>
+                  <h2>price</h2>
+                </div>
+              </div>
+            </div>
+
+            <button>delete</button>
+            <select value="" onChange="">
+              <option value="">1</option>
+              <option value="">2</option>
+              <option value="">3</option>
+              <option value="">4</option>
+            </select>
+          </div>
+          <div className="checkout-box">
+            <h1 className="title">Summary</h1>
+            <h3>total $...</h3>
+            <button>CONTINUE TO CHECKOUT</button>
+          </div>
+        </box>
       </div>
     );
   }
