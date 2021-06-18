28144283ca8d3e6ac7de43a93f65b28895c20ab5
a-hsu <a.hsu873@gmail.com>
2021-06-18 09:30:23 -0400

    feat: added isFulfilled column to Order model

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,10 +2,14 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const Order = db.define("order", {
-  total: {
-    type: Sequelize.DECIMAL,
-    defaultValue: 0,
-  },
+    total: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+    isFulfilled: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+    },
 });
 
 module.exports = Order;
