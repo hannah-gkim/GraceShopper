41481bb27ac14c6c0e926988dd17cd90dddf5343
hannahk0108 <hannahk0108@gmail.com>
2021-06-17 23:30:32 -0500

    inProgress: checkout-cart frontend CSS in progress

--- client/components/CheckoutCart.js
+++ client/components/CheckoutCart.js
@@ -9,27 +9,54 @@ class CheckoutCart extends Component {
         <box className="checkout-div">
           <div className="checkout-items-box">
             <h1 className="title">Shopping Cart</h1>
-
-            <div className="singeitem-image">
+            <hr />
+            <div className="singeitem-div">
               <img src="https://blog.williams-sonoma.com/wp-content/uploads/2018/06/jun-23-Vanilla-Ombre-Layer-Cake.jpg" />
               <div className="detail">
                 <h2>name</h2>
                 <h2>price</h2>
               </div>
+              <div className="addorremove">
+                <select value="" onChange="">
+                  <option value="">1</option>
+                  <option value="">2</option>
+                  <option value="">3</option>
+                  <option value="">4</option>
+                </select>
+                <button>delete</button>
+              </div>
             </div>
-
-            <button>delete</button>
-            <select value="" onChange="">
-              <option value="">1</option>
-              <option value="">2</option>
-              <option value="">3</option>
-              <option value="">4</option>
-            </select>
+            <div className="singeitem-div">
+              <img src="https://blog.williams-sonoma.com/wp-content/uploads/2018/06/jun-23-Vanilla-Ombre-Layer-Cake.jpg" />
+              <div className="detail">
+                <h2>name</h2>
+                <h2>price</h2>
+              </div>
+              <div className="addorremove">
+                <select value="" onChange="">
+                  <option value="">1</option>
+                  <option value="">2</option>
+                  <option value="">3</option>
+                  <option value="">4</option>
+                </select>
+                <button>delete</button>
+              </div>
+            </div>
+            <hr />
+            <div className="checkout">
+              <button>CONTINUE TO CHECKOUT</button>
+            </div>
           </div>
           <div className="checkout-box">
             <h1 className="title">Summary</h1>
-            <h3>total $...</h3>
-            <button>CONTINUE TO CHECKOUT</button>
+            <hr />
+            <div className="total">
+              <h3>SUBTOTAL $95</h3>
+              <h3>SHIPPING FREE</h3>
+              <h3>TAXES $10</h3>
+              <hr />
+              <h3>Total $100</h3>
+            </div>
           </div>
         </box>
       </div>
