f30db735a805736174603e341595079f032ab923
a-hsu <a.hsu873@gmail.com>
2021-06-21 16:49:16 -0400

    RequireToken gatekeeping middleware and added viewCart authentication

--- server/api/gatekeepingMiddleware.js
+++ server/api/gatekeepingMiddleware.js
@@ -3,10 +3,10 @@ const User = require("../db/models/user");
 const requireToken = async (req, res, next) => {
     try {
         const token = req.headers.authorization;
-        console.log("token", req.headers);
         const user = await User.findByToken(token);
         req.user = user;
         next();
+
     } catch (error) {
         next(error);
     }
--- server/api/users.js
+++ server/api/users.js
@@ -1,20 +1,52 @@
 const router = require("express").Router();
 const {
-    models: { User },
+    models: { User, Order },
 } = require("../db");
 const { requireToken } = require("./gatekeepingMiddleware");
 module.exports = router;
 
 router.get("/", requireToken, async (req, res, next) => {
     try {
-        const users = await User.findAll({
-            // explicitly select only the id and username fields - even though
-            // users' passwords are encrypted, it won't help if we just
-            // send everything to anyone who asks!
-            attributes: ["id", "username"],
-        });
-        res.json(users);
+        //TODO Only show all users IF req.user is an admin
+        console.log(req.user.isAdmin);
+        if (req.user.isAdmin) {
+            const users = await User.findAll({
+                // explicitly select only the id and username fields - even though
+                // users' passwords are encrypted, it won't help if we just
+                // send everything to anyone who asks!
+                attributes: ["id", "username"],
+            });
+            res.json(users);
+        } else {
+            res.status(403).send("You are not an admin");
+        }
     } catch (err) {
         next(err);
+    }
+});
+
+//Show User Profile
+router.get("/:id", async (req, res, next) => {});
+
+router.get("/:id/viewCart", requireToken, async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        console.log("req.user.id", typeof req.user.id);
+        console.log("/:id", typeof id);
+
+        if (req.user.id == id) {
+            const order = await Order.findAll({
+                where: {
+                    userId: id,
+                    isFulfilled: false,
+                },
+            });
+            console.log(order);
+            res.json(order);
+        } else {
+            res.json("incorrect id");
+        }
+    } catch (error) {
+        next(error);
     }
 });
