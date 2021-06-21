77f327a91730a606cf5580277a6ab0feabc28779
Eik <etty.ikpeme@gmail.com>
2021-06-21 13:34:42 -0500

    CSS on all Product View

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -4,55 +4,63 @@ import { getProducts } from "../store/allProducts";
 import { Link } from "react-router-dom";
 
 class AllProducts extends React.Component {
-    constructor(props) {
-        super(props);
-        // this.state = { products: [] };
-    }
+  constructor(props) {
+    super(props);
+    // this.state = { products: [] };
+  }
 
-    componentDidMount() {
-        this.props.loadAllProducts();
-    }
+  componentDidMount() {
+    this.props.loadAllProducts();
+  }
 
-    // componentDidUpdate(prevProps) {
-    //   //conditions
-    //   if (prevProps !== this.props) {
-    //     this.setState(this.props);
-    //   }
-    // }
-    render() {
-        console.log(this.props);
-        const { products } = this.props;
-        return (
-            <div>
-                <ul>
-                    {products
-                        ? products.map((product) => {
-                              return (
-                                  <Link
-                                      to={`/products/${product.id}`}
-                                      key={product.id}
-                                  >
-                                      <li> {product.name}</li>
-                                  </Link>
-                              );
-                          })
-                        : "no products"}
-                </ul>
-            </div>
-        );
-    }
+  // componentDidUpdate(prevProps) {
+  //   //conditions
+  //   if (prevProps !== this.props) {
+  //     this.setState(this.props);
+  //   }
+  // }
+
+  render() {
+    console.log(this.props);
+    const { products } = this.props;
+    return (
+      <ul>
+        <div className="products-container">
+          {products
+            ? products.map((product) => {
+                return (
+                  <div key={product.id} className="product">
+                    <Link to={`/products/${product.id}`}>
+                      <li> {product.name}</li>
+                    </Link>
+                    <img
+                      src={product.imageUrl}
+                      alt={product.id}
+                      width="400"
+                      height="auto"
+                    />
+                    <h4>{product.description} </h4>
+                    <h4>{product.price} </h4>
+                  </div>
+                );
+              })
+            : "no products"}
+        </div>
+      </ul>
+    );
+  }
 }
 
 const mapState = (state) => {
-    return {
-        products: state.products,
-    };
+  return {
+    products: state.products,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-    return {
-        loadAllProducts: () => dispatch(getProducts()),
-    };
+  return {
+    loadAllProducts: () => dispatch(getProducts()),
+  };
 };
 
 export default connect(mapState, mapDispatch)(AllProducts);
