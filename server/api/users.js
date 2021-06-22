c033683c3e2003bdcbf444df4cd62f6c62a18fb5
hannahk0108 <hannahk0108@gmail.com>
2021-06-22 10:31:52 -0500

    feat : cart item can be added to cartitem table

--- server/api/users.js
+++ server/api/users.js
@@ -24,48 +24,21 @@ router.post("/:id/cart", async (req, res, next) => {
     const { id } = req.params;
     const user = await User.findByPk(id);
     let order = await Order.findAll({
-      where: { userId: id },
+      where: { userId: id, isFulfilled: false },
     });
-
-    console.log("got the order-->", order);
-    console.log("got orderId-->", order[0].id);
-    console.log("got order.isFulfilled", order[0].isFulfilled);
-
+    // console.log("got the order-->", order);
+    // console.log("got orderId-->", order[0].id);
+    // console.log("got order.isFulfilled", order[0].isFulfilled);
     //TODO: still need to figure out adding product detail
-    if (order[0].isFulfilled === false) {
-      const cartItem = await CartItem.create({
-        quantity: 1,
-        pastPrice: 300,
-        currentPrice: 300,
-        orderId: order[0].id,
-        productId: 2,
-      });
-      return cartItem;
-    }
-    console.log("cartItem--->", cartItem);
-    res.sendStatus(200).json(cartItem);
+    const cartItem = await CartItem.create({
+      quantity: 1,
+      pastPrice: 300,
+      currentPrice: 300,
+      orderId: order[0].id,
+      productId: 2,
+    });
+    res.status(200).json(cartItem);
   } catch (error) {
     next(error);
   }
 });
-
-// //api/users/:userId/:productId
-// router.post("/:userId/:productId", async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const product = await Product.findByPk(id);
-//     //TODO: get correct quantity
-//     const cartItem = await CartItem.create({
-//       quantity: 1,
-//       // TODO: find a way to preserve past price
-//       pastPrice: product.price,
-//       currentPrice: product.price,
-//       orderId: 1, //user.id??
-//       productId: id,
-//     });
-//     // console.log("Purchased item > ", cartItem);
-//     res.sendStatus(200).json(cartItem);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
