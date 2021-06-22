13b46021542b9e206f2ae95f4bc7175009de4307
hannahk0108 <hannahk0108@gmail.com>
2021-06-22 00:35:25 -0500

    adding to cart making progress

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {
-  models: { User },
+  models: { User, CartItem, Order },
 } = require("../db");
 module.exports = router;
 
@@ -18,23 +18,53 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-//api/users/:userId/:productId
-router.post("/:userId/:productId", async (req, res, next) => {
+//api/users/:id/cart
+router.post("/:id/cart", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const product = await Product.findByPk(id);
-    //TODO: get correct quantity
-    const cartItem = await CartItem.create({
-      quantity: 1,
-      // TODO: find a way to preserve past price
-      pastPrice: product.price,
-      currentPrice: product.price,
-      orderId: 1, //user.id??
-      productId: id,
+    const user = await User.findByPk(id);
+    let order = await Order.findAll({
+      where: { userId: id },
     });
-    // console.log("Purchased item > ", cartItem);
+
+    console.log("got the order-->", order);
+    console.log("got orderId-->", order[0].id);
+    console.log("got order.isFulfilled", order[0].isFulfilled);
+
+    if (order[0].isFulfilled === false) {
+      const cartItem = await CartItem.create({
+        quantity: 1,
+        pastPrice: 300,
+        currentPrice: 300,
+        orderId: order[0].id,
+        productId: 2,
+      });
+      return cartItem;
+    }
+    console.log("cartItem--->", cartItem);
     res.sendStatus(200).json(cartItem);
   } catch (error) {
     next(error);
   }
 });
+
+// //api/users/:userId/:productId
+// router.post("/:userId/:productId", async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await Product.findByPk(id);
+//     //TODO: get correct quantity
+//     const cartItem = await CartItem.create({
+//       quantity: 1,
+//       // TODO: find a way to preserve past price
+//       pastPrice: product.price,
+//       currentPrice: product.price,
+//       orderId: 1, //user.id??
+//       productId: id,
+//     });
+//     // console.log("Purchased item > ", cartItem);
+//     res.sendStatus(200).json(cartItem);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
