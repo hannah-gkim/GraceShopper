e0f12e280f3bb4b4250d335936578c3b4cde13bf
Eik <etty.ikpeme@gmail.com>
2021-06-23 11:15:09 -0500

    Admin feat: make backend requests to add, edit, and remove products

--- server/api/products.js
+++ server/api/products.js
@@ -26,42 +26,57 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+// POST api/products/
 router.post("/", requireToken, async (req, res, next) => {
   try {
-      //TODO Only show all users IF req.user is an admin
-      // console.log("am i an admin?", req.user.isAdmin);
-      if (req.user.isAdmin) {
-          const users = await User.findAll({
-              // explicitly select only the id and username fields - even though
-              // users' passwords are encrypted, it won't help if we just
-              // send everything to anyone who asks!
-              attributes: ["id", "username"],
-          });
-          res.json(users);
-      } else {
-          res.status(403).send("You are not an admin");
-      }
+    //TODO Only show all users IF req.user is an admin
+    // console.log("am i an admin?", req.user.isAdmin);
+    if (req.user.isAdmin) {
+      //Form used for post request should validate for all fields
+      const product = await Product.create(req.body);
+      res.json(product);
+    } else {
+      res.status(403).send("You are not an admin");
+    }
   } catch (err) {
-      next(err);
+    next(err);
   }
 });
 
-// router.post("/:id", async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const product = await Product.findByPk(id);
-//     //TODO: get correct quantity
-//     const cartItem = await CartItem.create({
-//       quantity: 1,
-//       // TODO: find a way to preserve past price
-//       pastPrice: product.price,
-//       currentPrice: product.price,
-//       orderId: 1,
-//       productId: id,
-//     });
-//     console.log("Purchased item > ", cartItem);
-//     res.sendStatus(200).json(cartItem);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+// DELETE /products/id
+router.delete("/:id", requireToken, async (req, res, next) => {
+  try {
+    //TODO Only show all users IF req.user is an admin
+    // console.log("am i an admin?", req.user.isAdmin);
+    if (req.user.isAdmin) {
+      const product = await Product.findByPk(req.params.id);
+      if (!product) return res.sendStatus(404);
+      res.json(product).status(204);
+      product.destroy();
+    } else {
+      res.status(403).send("You are not an admin");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT /products/id
+router.put("/:id", requireToken, async (req, res, next) => {
+  try {
+    //TODO Only show all users IF req.user is an admin
+    // console.log("am i an admin?", req.user.isAdmin);
+    if (req.user.isAdmin) {
+      const product = await Product.findByPk(req.params.id);
+      if (!product) return res.sendStatus(404);
+      //Form used for put update request should validate for all fields
+      product.update(req.body)
+      res.json(product);
+    } else {
+      res.status(403).send("You are not an admin");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
