fa3406cad465e4567a868a4e9ab86eb9cef68e82
hannahk0108 <hannahk0108@gmail.com>
2021-06-23 21:15:05 -0500

    added to cart quick message page

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -2,84 +2,87 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchSingleProduct } from "../store/singleProduct";
 import { getNewCartItem } from "../store/cartitem";
+import { Link } from "react-router-dom";
 
 class SingleProduct extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { quantity: 1 };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleAddToCart = this.handleAddToCart.bind(this);
-    }
-    componentDidMount() {
-        this.props.loadProduct(this.props.match.params.id);
-    }
-    // componentWillUnmount() {
-    //     this.props.student.id = undefined;
-    // }
-    handleChange(event) {
-        event.preventDefault();
-        console.log("this is quantity", event.target.value);
-        const quantity = event.target.value || 1;
-        this.setState({ quantity });
-    }
+  constructor(props) {
+    super(props);
+    this.state = { quantity: 1 };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAddToCart = this.handleAddToCart.bind(this);
+  }
+  componentDidMount() {
+    this.props.loadProduct(this.props.match.params.id);
+  }
+  // componentWillUnmount() {
+  //     this.props.student.id = undefined;
+  // }
+  handleChange(event) {
+    event.preventDefault();
+    console.log("this is quantity", event.target.value);
+    const quantity = event.target.value || 1;
+    this.setState({ quantity });
+  }
 
-    handleAddToCart() {
-        const cartitem = {
-            productId: this.props.match.params.id, //productID
-            quantity: this.state.quantity,
-        };
-        const userId = this.props.user.id;
-        this.props.createCartItem(userId, cartitem);
-    }
-    render() {
-        const product = this.props.singleProduct || {};
-        // const quantity = this.props.singleProduct.quantity;
-        // does cartitem need product id only or more?
-        let price = product.price / 100;
-        var formatter = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-        });
-        price = formatter.format(price);
-        return (
-            <div>
-                <h1>Single Product Page</h1>
-                <img src={product.imageUrl} />
-                <h3>name: {product.name}</h3>
-                <h3>price: {`${price}`}</h3>
-                <h3>description: {product.description}</h3>
+  handleAddToCart() {
+    const cartitem = {
+      productId: this.props.match.params.id, //productID
+      quantity: this.state.quantity,
+    };
+    const userId = this.props.user.id;
+    this.props.createCartItem(userId, cartitem);
+  }
+  render() {
+    const product = this.props.singleProduct || {};
+    // const quantity = this.props.singleProduct.quantity;
+    // does cartitem need product id only or more?
+    let price = product.price / 100;
+    var formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    price = formatter.format(price);
+    return (
+      <div>
+        <h1>Single Product Page</h1>
+        <img src={product.imageUrl} />
+        <h3>name: {product.name}</h3>
+        <h3>price: {`${price}`}</h3>
+        <h3>description: {product.description}</h3>
 
-                <select id="quantity" onChange={this.handleChange}>
-                    <option value="1">Qty:1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                </select>
-                <button type="button" onClick={this.handleAddToCart}>
-                    add to cart
-                </button>
-            </div>
-        );
-    }
+        <select id="quantity" onChange={this.handleChange}>
+          <option value="1">Qty:1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+        <Link to="/addedToCart">
+          <button type="button" onClick={this.handleAddToCart}>
+            add to cart
+          </button>
+        </Link>
+      </div>
+    );
+  }
 }
 
 const mapState = (state) => {
-    return {
-        singleProduct: state.singleProduct,
-        isLoggedIn: !!state.auth.id,
-        user: state.auth,
-    };
+  return {
+    singleProduct: state.singleProduct,
+    isLoggedIn: !!state.auth.id,
+    user: state.auth,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-    return {
-        loadProduct: (id) => {
-            dispatch(fetchSingleProduct(id));
-        },
-        createCartItem: (userId, cartitem) => {
-            dispatch(getNewCartItem(userId, cartitem));
-        },
-    };
+  return {
+    loadProduct: (id) => {
+      dispatch(fetchSingleProduct(id));
+    },
+    createCartItem: (userId, cartitem) => {
+      dispatch(getNewCartItem(userId, cartitem));
+    },
+  };
 };
 
 export default connect(mapState, mapDispatch)(SingleProduct);
