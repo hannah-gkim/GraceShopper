84cf5ddc921fd78683cc6bfad4458494228bcd6f
hannahk0108 <hannahk0108@gmail.com>
2021-08-17 01:32:56 -0500

    working on header

--- client/App.js
+++ client/App.js
@@ -4,21 +4,9 @@ import Routes from "./Routes";
 
 const App = () => {
   return (
-    <div className="main-div">
-      <div className="navbar-top">
-        <div className="p-div">
-          <p>
-            FREE shipping on orders $75+ | Register for FREE Shipping on Your
-            1st Order
-          </p>
-        </div>
-      </div>
-
+    <div>
       <Navbar />
-      <hr></hr>
-      <div className="content-div">
-        <Routes />
-      </div>
+      <Routes />
     </div>
   );
 };
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,36 +2,76 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
+import "boxicons";
 
 //  state,  thunk
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <nav className="nav">
-    <div className="nav-wrap">
-      <div className="nav-logo">
-        <Link to="/">Les Choses de La Vie</Link>
-      </div>
-      {isLoggedIn ? (
-        <div className="nav-items">
-          <Link to="/products">Shop</Link>
-          <Link to={`/viewCart`}>Cart</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
+const Navbar = ({ handleClick, isLoggedIn }) => {
+  function showMenu() {
+    console.log("clicked");
+    
+  }
+  return (
+    <header className="header1">
+      <nav className="nav bd-grid">
         <div>
-          {/*if not loggedIn*/}
-          <div className="nav-items">
-            <Link to="/products">Shop</Link>
-            <Link to={`/viewCart`}>Cart</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
+          <Link to="/" className="nav__logo">
+            Les Choses de La Vie
+          </Link>
         </div>
-      )}
-    </div>
-  </nav>
-);
+
+        {isLoggedIn ? (
+          <div className="nav__menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <Link to="/products" className="nav__link">
+                  Shop
+                </Link>
+              </li>
+              <li className="nav__item">
+                <a href="#" onClick={handleClick} className="nav__link">
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div className="nav__menu">
+            {/*if not loggedIn*/}
+            <ul className="nav__list">
+              <li className="nav__item">
+                <Link to="/products" className="nav__link">
+                  Shop
+                </Link>
+              </li>
+
+              <li className="nav__item">
+                <Link to="/login" className="nav__link">
+                  Login
+                </Link>
+              </li>
+              <li className="nav__item">
+                <Link to="/signup" className="nav__link">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+        <div>
+          <Link to={`/viewCart`}>
+            <box-icon name="cart" className="nav__cart"></box-icon>
+          </Link>
+
+          <box-icon
+            onClick={showMenu}
+            className="nav__toggle"
+            name="menu"
+          ></box-icon>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 /* CONTAINER */
 const mapState = (state) => {
