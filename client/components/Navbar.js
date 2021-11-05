678f188635ae431b6f89534a72c4106d892b8dca
hannahk0108 <hannahk0108@gmail.com>
2021-11-05 12:38:38 -0500

    navBar outside click handler added

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
 import "boxicons";
+import OutsideClickHandler from "react-outside-click-handler";
 
 //  state,  thunk
 function Navbar({ isLoggedIn, handleClick }) {
@@ -11,72 +12,79 @@ function Navbar({ isLoggedIn, handleClick }) {
   const toggleMenu = () => {
     setIsActive(!isActive);
   };
-
   const hideMenu = () => {
     setIsActive("false");
   };
 
   return (
     <header className="header1">
-      <nav className="nav bd-grid">
-        <div>
-          <Link to="/" className="nav__logo" onClick={hideMenu}>
-            Les Choses de La Vie
-          </Link>
-        </div>
-        {/* when active==='false' mobile side menu disappears*/}
-        <div className={isActive ? "nav__menu" : "show"}>
-          <ul className="nav__list">
-            <li className="nav__item">
-              <Link to="/" className="nav__link" onClick={hideMenu}>
-                Home
-              </Link>
-            </li>
+      <OutsideClickHandler
+        onOutsideClick={() => {
+          setIsActive("false");
+        }}
+      >
+        <nav className="nav bd-grid">
+          {/************* logo *************/}
+          <div>
+            <Link to="/" className="nav__logo" onClick={hideMenu}>
+              Les Choses de La Vie
+            </Link>
+          </div>
+          {/* onClick of menu, isActive=false */}
+          <div className={isActive ? "nav__menu" : "show"}>
+            {console.log(isActive)}
+            <ul className="nav__list">
+              <li className="nav__item">
+                <Link to="/" className="nav__link" onClick={hideMenu}>
+                  Home
+                </Link>
+              </li>
 
-            <li className="nav__item">
-              <Link to="/products" className="nav__link" onClick={hideMenu}>
-                Shop
-              </Link>
-            </li>
-            {/***************** cart *****************/}
-            {/* <li className="nav__item">
+              <li className="nav__item">
+                <Link to="/products" className="nav__link" onClick={hideMenu}>
+                  Shop
+                </Link>
+              </li>
+              {/***************** cart *****************/}
+              {/* <li className="nav__item">
               <Link to={`/viewCart`} className="nav__link" onClick={hideMenu}>
                 Cart
               </Link>
             </li> */}
 
-            {isLoggedIn ? (
-              <li className="nav__item" onClick={hideMenu}>
-                <Link to="/" onClick={handleClick} className="nav__link">
-                  Logout
-                </Link>
-              </li>
-            ) : (
-              <>
-                <li className="nav__item active">
-                  <Link to="/login" className="nav__link" onClick={hideMenu}>
-                    Login
+              {isLoggedIn ? (
+                <li className="nav__item" onClick={hideMenu}>
+                  <Link to="/" onClick={handleClick} className="nav__link">
+                    Logout
                   </Link>
                 </li>
-                <li className="nav__item active">
-                  <Link to="/signup" className="nav__link" onClick={hideMenu}>
-                    Sign Up
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-
-        <div className="cart-menu-icon">
-          <Link to={`/viewCart`} onClick={hideMenu}>
-            <box-icon name="cart" className="nav__cart"></box-icon>
-          </Link>
-          <div className="nav__toggle " onClick={toggleMenu}>
-            <box-icon name="menu"></box-icon>
+              ) : (
+                <>
+                  <li className="nav__item active">
+                    <Link to="/login" className="nav__link" onClick={hideMenu}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav__item active">
+                    <Link to="/signup" className="nav__link" onClick={hideMenu}>
+                      Sign Up
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
           </div>
-        </div>
-      </nav>
+
+          <div className="cart-menu-icon">
+            <Link to={`/viewCart`} onClick={hideMenu}>
+              <box-icon name="cart" className="nav__cart"></box-icon>
+            </Link>
+            <div className="nav__toggle " onClick={toggleMenu}>
+              <box-icon name="menu"></box-icon>
+            </div>
+          </div>
+        </nav>
+      </OutsideClickHandler>
     </header>
   );
 }
